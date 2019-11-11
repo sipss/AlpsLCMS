2d5553d4f7a6efd1ec794b47993cfdb8285cf994
Luis-Fernandez-Romero <lfernrom@gmail.com>
2019-11-11 17:22:54 +0100

    lcms_align_Rtime with examples
    lcms_align_Rtime with examples (change of name)
     - inclusion of one real example

--- R/Retention_Time_Correction.R
+++ R/Retention_Time_Correction.R
@@ -3,26 +3,40 @@
 #' Retention time correction is performed using *'obiwarp'* method.
 #' Its optimum parameters are obtaine from `IPO` Package.
 #' Note: signal processing generally  consists in three main steps:
-#' (1) peak detection (`findChromPeaks_cwp` function),
-#' (2) peak alignment (`align_Rtime` function) and
-#' (3) peak correspondence (`group_peaks` function).
+#' (1) peak detection (`lcms_findChromPeaks_cwp` function),
+#' (2) peak alignment (`lcms_align_Rtime` function) and
+#' (3) peak correspondence (`lcms_group_peaks` function).
 #'
 #' @param peakdet A lcms_dataset with detected peaks from the
 #' `findChromPeaks_cwp` function
 #' @param params A converted parameters template from IPO parameters.
 #' @examples
-#' \dontrun{
-#' params <- convert_IPO_to_XCMS(IPO_params)
-#' peakdet <- findChromPeaks_cwp(dataset, params = params)
-#' peakdet_align <- align_Rtime(peakdet, params = params)
-#' }
+#' file_name <-  system.file("extdata", "peakdet.rds", package = "NIHSlcms")
+#' peakdet <- base::readRDS(file_name)
+#' opt_result_path <-  system.file("extdata", package = "NIHSlcms")
+#' lcms_preproc_params <- lcms_read_ipo_to_xcms(opt_result_path)
 #'
-#' @return A lcms_dataset with (1) detected (`findChromPeaks_cwp` function)
-#' and (2) aligned (`align_Rtime` function) peaks
+#' peakdet_align <- lcms_align_Rtime(peakdet, params = lcms_preproc_params)
+#' print(peakdet_align)
+#'
+#' @return A lcms_dataset with (1) detected (`lcms_findChromPeaks_cwp` function)
+#' and (2) aligned (`lcms_align_Rtime` function) peaks
 #' @export
 #'
-align_Rtime <- function (peakdet, params) {
-  obiwarp_params <- xcms::ObiwarpParam(binSize = params$profStep,
+lcms_align_Rtime <- function (peakdet, params) {
+
+  quiet <- function(x) {
+      base::sink(base::tempfile())
+      base::on.exit(base::sink())
+      base::invisible(base::force(x))
+    }
+
+  cat("Aligning the peak table using the optimized set of parameters
+        obtained from IPO package.","\n")
+
+  obiwarp_params <- base::suppressWarnings(
+                            base::suppressMessages(
+                                    quiet(xcms::ObiwarpParam(binSize = params$profStep,
                                      centerSample = params$centerSample,
                                      response = params$response,
                                      distFun = params$distFun,
@@ -31,8 +45,14 @@ align_Rtime <- function (peakdet, params) {
                                      factorDiag = params$factorDiag,
                                      factorGap = params$factorGap,
                                      localAlignment = params$localAlignment,
-                                     initPenalty = params$initPenalty)
+                                     initPenalty = params$initPenalty))
+                                                  )
+                                           )
 
-  peakdet_align <- xcms::adjustRtime(peakdet, param = obiwarp_params)
+  peakdet_align <-  base::suppressWarnings(
+                            base::suppressMessages(
+                                    quiet(xcms::adjustRtime(peakdet, param = obiwarp_params))
+                                                  )
+                                          )
   return(peakdet_align)
 }
