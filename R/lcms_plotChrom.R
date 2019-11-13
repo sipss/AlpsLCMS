541a0fccad9609e6f6e18180cc7c7f6f5cde983b
Luis-Fernandez-Romero <lfernrom@gmail.com>
2019-11-13 18:49:31 +0100

    lcms_plotChrom change variable name
    we changed chromatogram for  chromatogram_object

--- R/lcms_plotChrom.R
+++ R/lcms_plotChrom.R
@@ -2,7 +2,7 @@
 #'
 #' Base peak chromatograms with retention time axis in minutes.
 #'
-#' @param chromatogram  A XChromatograms object
+#' @param chromatogram_object  A XChromatograms object
 #' @param treatment_col Color code by groups. They can be generated using
 #' treatment_col <- scales::hue_pal()(length(unique(lcms_dataset$treatment)))
 #' names(treatment_col) <- unique(lcms_dataset$treatment)
@@ -23,16 +23,16 @@
 #' lcms_plotChrom(base_peaks, treatment_col, rtlim = c(4, 14))
 #'
 #'
-lcms_plotChrom <- function (chromatogram, treatment_col, rtlim = NULL){
+lcms_plotChrom <- function (chromatogram_object, treatment_col, rtlim = NULL){
   min2sec <- 60
   message("Make sure that the column that contains the group class is called `treatment`")
 
   #we need to modify this in order to be more flexible (treatment_col)
-  ret_times <- lapply(chromatogram, FUN = rtime)
-  intensities <- lapply(chromatogram, FUN = intensity)
+  ret_times <- lapply(chromatogram_object, FUN = rtime)
+  intensities <- lapply(chromatogram_object, FUN = intensity)
 
   plot(ret_times[[1]] / min2sec, intensities[[1]], type = "l",
-       col = treatment_col[chromatogram$treatment][1], lwd = 1,
+       col = treatment_col[chromatogram_object$treatment][1], lwd = 1,
        xlab = "Retention time (min)", ylab = "Intensity  (A.U.)",
        xlim = rtlim,
        main = "Base Peak Chromatogram")
@@ -40,7 +40,7 @@ lcms_plotChrom <- function (chromatogram, treatment_col, rtlim = NULL){
   for (i in 2:length(ret_times)){ # we need to modify the sequence using seq_along
     points(ret_times[[i]] / min2sec, intensities[[i]], lwd = 1,
            xlim = rtlim,
-           type= "l", col = treatment_col[chromatogram$treatment][i])
+           type= "l", col = treatment_col[chromatogram_object$treatment][i])
     legend("topright", legend = names(treatment_col), fill = treatment_col)
   }
 }
