1e97e53368c82e609d0fecf4428508d6049849c3
Luis-Fernandez-Romero <lfernrom@gmail.com>
2019-12-09 12:12:42 +0100

    update lcms_dataset family
    update lcms_dataset family

--- R/lcms_dataset_family.R
+++ R/lcms_dataset_family.R
@@ -7,6 +7,6 @@
 #'
 #' @name lcms_dataset_family
 #' @family NIHSnmr dataset objects
-#' @seealso [Functions to save and load these objects][load_and_save_functions]
+#' @seealso [sample_management][pre_processing]
 #NULL
 
