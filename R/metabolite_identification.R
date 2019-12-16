f252668ca1895ff5bb485cfc43aa9b74d46bff16
Luis-Fernandez-Romero <lfernrom@gmail.com>
2019-12-16 10:59:43 +0100

    lcms_spectral_sig_features -> stats:median
    if you don't include stats:median inside stats::aggregate function lcms_spectral_sig_features does not work at all

--- R/metabolite_identification.R
+++ R/metabolite_identification.R
@@ -624,11 +624,11 @@ lcms_sig_peaks_table<-function(
     stop("No MAIT object was given")
   }
 
-  if(length(MAIT::featureSigID(MAIT.object))==0){
+  if(length(featureSigID(MAIT.object))==0){
     stop("No significant features found in the MAIT object. Make sure that functions peakAnnotation and spectralSigFeatures were launched")
   }
 
-  dat <- MAIT::getScoresTable(MAIT.object,getSpectra=TRUE,getExtendedTable=TRUE)
+  dat <- getScoresTable(MAIT.object,getSpectra=TRUE,getExtendedTable=TRUE)
 
   if(printAnnotation==TRUE){extendedTable<-TRUE}
 
@@ -639,19 +639,21 @@ lcms_sig_peaks_table<-function(
   }
   spectraID <- dat$spectraID
 
-  data <- MAIT::scores(MAIT.object)
-  index <- MAIT::featureSigID(MAIT.object)
-  classes <- MAIT::classes(MAIT.object)
+
+  data <- scores(MAIT.object)
+  index <- featureSigID(MAIT.object)
+  classes <- classes(MAIT.object)
   classNum <- classNum(MAIT.object)
   resultsPath <- MAIT.object@PhenoData@resultsPath#resultsPath(MAIT.object)
-  Fisher <- MAIT::LSDResults(MAIT.object)
-  TTs <- MAIT::pvalues(MAIT.object)
+  Fisher <- LSDResults(MAIT.object)
+  TTs <- pvalues(MAIT.object)
 
   sigPeaksTable <- matrix(nrow=1,ncol=ncol(peakList))
   colnames(sigPeaksTable) <- colnames(peakList)
 
+
   if (length(index)!=0){
-    if (MAIT::method(MAIT.object)!="None"){
+    if (method(MAIT.object)!="None"){
       sigPeaksTable <- peakList[spectraID%in%index,]
     }else{
       sigPeaksTable <- peakList[spectraID%in%unique(spectraID[index]),]
@@ -660,8 +662,8 @@ lcms_sig_peaks_table<-function(
 
     p <- matrix(nrow=1,ncol=dim(sigPeaksTable)[1])
     fisher <- matrix(nrow=1,ncol=dim(sigPeaksTable)[1])
-    if(class(MAIT::classes(MAIT.object))!="logical"|class(classNum(MAIT.object))!="logical"){
-      if(length(MAIT::classes(MAIT.object))>2){
+    if(class(classes(MAIT.object))!="logical"|class(classNum(MAIT.object))!="logical"){
+      if(length(classes(MAIT.object))>2){
         for(i in c(1:dim(sigPeaksTable)[1])){
           fisher[i] <- Fisher[as.numeric(sigPeaksTable[i,dim(sigPeaksTable)[2]])]
         }
@@ -680,18 +682,19 @@ lcms_sig_peaks_table<-function(
       }
     }
 
-    p<-MAIT::pvalues(MAIT.object)
+
+    p<-pvalues(MAIT.object)
     p <- p[spectraID%in%unique(spectraID[index])]
 
     p <- matrix(p,ncol=1)
     if(MAIT.object@FeatureData@pvaluesCorrection==""){MAIT.object@FeatureData@pvaluesCorrection<-"none"}
-    P.adjust <- stats::p.adjust(p,MAIT.object@FeatureData@pvaluesCorrection)
+    P.adjust <- p.adjust(p,MAIT.object@FeatureData@pvaluesCorrection)
 
     pAux <- matrix(ncol=1,nrow=dim(sigPeaksTable)[1])
     pAux.adjust <- matrix(ncol=1,nrow=dim(sigPeaksTable)[1])
 
 
-    if (MAIT::method(MAIT.object)!="None"){
+    if (method(MAIT.object)!="None"){
       for (i in c(1:dim(sigPeaksTable)[1])){
         if(sum(sigPeaksTable$pcgroup==index)==length(index)){
           pAux[i,] <- unique(p[which(index%in%index[i])])
@@ -710,7 +713,7 @@ lcms_sig_peaks_table<-function(
     sigPeaksTable <- cbind(sigPeaksTable,pAux.adjust,pAux)
 
     sigPeaksTable <- cbind(sigPeaksTable,matrix(fisher,ncol=1))
-    if(length(MAIT::classes(MAIT.object))>2){
+    if(length(classes(MAIT.object))>2){
       colnames(sigPeaksTable)[dim(sigPeaksTable)[2]] <- NamesFisher
     }else{
       colnames(sigPeaksTable)[dim(sigPeaksTable)[2]] <- "Fisher.Test"
@@ -719,7 +722,8 @@ lcms_sig_peaks_table<-function(
     colnames(sigPeaksTable)[dim(sigPeaksTable)[2]-1] <- "p"
 
 
-    if(length(MAIT::rawData(MAIT.object))==0&is.null(sigPeaksTable$adduct)==TRUE){
+
+    if(length(rawData(MAIT.object))==0&is.null(sigPeaksTable$adduct)==TRUE){
       adduct <- character(length=dim(peakList)[1])
       peakList <- data.frame(peakList,adduct)
       adduct <- character(length=dim(sigPeaksTable)[1])
@@ -748,20 +752,20 @@ lcms_sig_peaks_table<-function(
     means <- vector("list",length=length(index))
     medians <- vector("list",length=length(index))
 
-    if(class(MAIT::classes(MAIT.object))!="logical"|class(classNum(MAIT.object))!="logical"){
-      Fgroups <- as.factor(rep(MAIT::classes(MAIT.object),classNum(MAIT.object)))
+    if(class(classes(MAIT.object))!="logical"|class(classNum(MAIT.object))!="logical"){
+      Fgroups <- as.factor(rep(classes(MAIT.object),classNum(MAIT.object)))
       for(i in c(1:dim(sigPeaksTable)[1])){
-        if(length(MAIT::rawData(MAIT.object))==0){
+        if(length(rawData(MAIT.object))==0){
           ind <- c(3:(dim(sigPeaksTable)[2]-5))
         }else{
-          ind <- c((8+length(MAIT::classes(MAIT.object))):(dim(sigPeaksTable)[2]-6))
+          ind <- c((8+length(classes(MAIT.object))):(dim(sigPeaksTable)[2]-6))
         }
-        means[[i]] <- stats::aggregate(as.numeric(sigPeaksTable[i,ind])~Fgroups,FUN=mean)
-        medians[[i]] <- stats::aggregate(as.numeric(sigPeaksTable[i,ind])~Fgroups,FUN=median)
+        means[[i]] <- stats::aggregate(as.numeric(sigPeaksTable[i,ind])~Fgroups,FUN=base::mean)
+        medians[[i]] <- stats::aggregate(as.numeric(sigPeaksTable[i,ind])~Fgroups,FUN=stats::median)
       }
 
       allMeans <- merge(means[[1]],means[[2]],by="Fgroups")
-      if(length(MAIT::rawData(MAIT.object))!=0){
+      if(length(rawData(MAIT.object))!=0){
         colnames(allMeans)[2:dim(allMeans)[2]] <- rownames(dat$scores[spectraID%in%unique(spectraID[index]),])[c(1,2)]
       }else{
         colnames(allMeans)[2:dim(allMeans)[2]] <- 1:(dim(allMeans)[2]-1)
@@ -769,7 +773,7 @@ lcms_sig_peaks_table<-function(
       if(length(index)>2){
         for(i in c(3:length(means))){
           allMeans <- merge(allMeans,means[[i]],by="Fgroups")
-          if(length(MAIT::rawData(MAIT.object))!=0){
+          if(length(rawData(MAIT.object))!=0){
             colnames(allMeans)[i+1] <-  rownames(dat$scores[spectraID%in%unique(spectraID[index]),])[i]
           }else{
             colnames(allMeans)[i+1]  <- as.numeric(colnames(allMeans)[i])+1
@@ -779,13 +783,13 @@ lcms_sig_peaks_table<-function(
       rownames(allMeans)<-paste("Mean Class",allMeans[,1])
 
       temp<-as.data.frame(t(allMeans[,-1]))
-      for(i in c(1:length(MAIT::classes(MAIT.object)))){
+      for(i in c(1:length(classes(MAIT.object)))){
         temp[,i]<-as.numeric(as.character(temp[,i]))
       }
 
-      if(length(MAIT::rawData(MAIT.object))!=0){rownames(temp)<-NULL}
+      if(length(rawData(MAIT.object))!=0){rownames(temp)<-NULL}
       allMedians <- merge(medians[[1]],medians[[2]],by="Fgroups")
-      if(length(MAIT::rawData(MAIT.object))!=0){
+      if(length(rawData(MAIT.object))!=0){
         colnames(allMedians)[2:dim(allMedians)[2]] <- rownames(dat$scores[spectraID%in%unique(spectraID[index]),])[c(1,2)]
       }else{
         colnames(allMedians)[2:dim(allMedians)[2]] <- 1:(dim(allMedians)[2]-1)
@@ -793,7 +797,7 @@ lcms_sig_peaks_table<-function(
       if(length(index)>3){
         for(i in c(3:length(medians))){
           allMedians <- merge(allMedians,medians[[i]],by="Fgroups")
-          if(length(MAIT::rawData(MAIT.object))!=0){
+          if(length(rawData(MAIT.object))!=0){
             colnames(allMedians)[i+1] <-  rownames(dat$scores[spectraID%in%unique(spectraID[index]),])[i]
           }else{
             colnames(allMedians)[i+1]  <- as.numeric(colnames(allMedians)[i])+1
@@ -804,11 +808,11 @@ lcms_sig_peaks_table<-function(
       rownames(allMedians)<-paste("Median Class",allMedians[,1])
 
       tempMed<-as.data.frame(t(allMedians[,-1]))
-      for(i in c(1:length(MAIT::classes(MAIT.object)))){
+      for(i in c(1:length(classes(MAIT.object)))){
         tempMed[,i]<-as.numeric(as.character(tempMed[,i]))
       }
       sigPeaksTable <- cbind(sigPeaksTable,temp,tempMed)
-      if(length(MAIT::rawData(MAIT.object))!=0){rownames(tempMed)<-NULL}
+      if(length(rawData(MAIT.object))!=0){rownames(tempMed)<-NULL}
     }else{
       temp <- matrix(rep(NA,dim(sigPeaksTable)[1]),ncol=1)
       tempMed <- matrix(rep(NA,dim(sigPeaksTable)[1]),ncol=1)
@@ -1645,7 +1649,7 @@ lcms_peak_table_boxplots <- function (MAIT.object = NULL, treatment_col) {
 #' @return A MAIT.object. Additionaly: Three different PCA scoreplots are printed in three png files.
 #' One using PC1 vs PC2, another with PC1 vs PC3 and the last one with PC2 vs PC3.
 #' The files will be stored in the directory /PCA_Scoreplots.
-#' #' @family metabolite identification functions
+#' @family metabolite identification functions
 #' @family dataset_peak_table functions
 #' @family statistical functions
 #' @family visualization functions
