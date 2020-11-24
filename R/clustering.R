aec7c216001f57eea2a950e934f68602e4eb87cd
Francisco Madrid-Gambin <francisco.madrid.gambin@gmail.com>
2020-11-24 19:18:37 +0100

    Documentation XII

--- R/clustering.R
+++ R/clustering.R
@@ -1,10 +1,10 @@
 #' Get st parameter value for clustering
 #'
-#' getRamSt calculates the optimal value for the [AlpsLCMS::clustering]
-#' function. It uses the `lcms_dataset` after xcms preprocessing.
+#' `getRamSt` function calculates the optimal value for the parameter `st` the [AlpsLCMS::clustering]
+#' function. It uses the `lcms_dataset` after the xcms preprocessing.
 #'
 #' @param lcms_dataset A lcms_dataset
-#' @return recommended st parameter value for [AlpsLCMS::clustering]
+#' @return the recommended value for the `st` parameter in [AlpsLCMS::clustering]
 #' @export
 getRamSt <- function(lcms_dataset) {
   featInfo <- xcms::featureDefinitions(lcms_dataset)
@@ -106,62 +106,6 @@ do_findmain <- function(...){
   RC
 }
 
-
-#' Labelled fragments
-#'
-#' `labelling` is a function that applied to the `hclust` object from
-#' [clustering] and [do_findmain], creates a list with extracted info from each
-#' cluster/pseudo-spectrum.
-#'
-#' @param RC hclust object from the [do_findmain] function.
-#'
-#' @return A list containing data frames with (1) all labelled adducts, (2)
-#'   representative ion in each cluster/pseudospectrum, and (3) the clustering
-#'   data.
-#'   $representative_ions: data frame containing the mz, relative intensity of the
-#'   fragment in the corresponding cluster, charge, selected adduct and ppm.
-#'   $cluster_data: data frame with info of the clustering, name of the
-#'   clusters, the most intense ion in the cluster, labelled ion, retention
-#'   time.
-#'
-#' @export
-#'
-labelling <- function (RC) {
-  # Selection of max intensity ion as cluster representative
-  Max_int<- lapply(RC$M.ann, function(x) x[which.max(x$int), ])
-  representative_ions <- dplyr::bind_rows(Max_int)
-  representative_ions$name <- paste(round(representative_ions$mz,4),
-                                    round(RC$clrt,2),
-                                    sep = "_")
-
-  # Abducts of representative ions
-  Representative_adducts <- sapply(RC$M.ann, function(x) x[which.max(x$int), ]$adduct)
-
-  # Selection of labelled max intensity ion as cluster representative
-  Labelled_int <- lapply(RC$M.ann, function(x) {
-    xl <- x[which(!is.na(x$label)), ]
-    xl[which.max(xl$int), ]
-  })
-  labelled_ions <- dplyr::bind_rows(Labelled_int)
-
-  # We save most important cluster data
-  cluster_data <- data.frame(cluster = RC$ann,
-                             Max_int_ion_mz = representative_ions$mz,
-                             Max_int_ion_adduct = Representative_adducts,
-                             Labelled_ion_mz = labelled_ions$mz,
-                             Labelled_ion_adduct = labelled_ions$label,
-                             RC_mz = RC$M,
-                             retention_time = RC$clrt)
-
-  # All labelled ions
-  all_labelled_adducts <- lapply(RC$M.ann, function(x) {
-    xl <- x[which(!is.na(x$label)), ]
-  })
-  all_labelled_adducts <- dplyr::bind_rows(all_labelled_adducts)
-  return(list(all_labelled_adducts = all_labelled_adducts, labelled_ions = labelled_ions, representative_ions = representative_ions, cluster_data = cluster_data))
-}
-
-
 #' Feature reduction
 #'
 #' `feature_reduction` is a function to reduce the feature table using the info
@@ -169,60 +113,162 @@ labelling <- function (RC) {
 #' cluster discarding the rest of the feature of the same cluster (normally, the
 #' most intense one).
 #'
-#' @param mdata data matrix with samples in rows and features in columns
-#' @param representative_ions data frame from the list given by [labelling] function
 #' @param RC hclust object from the [do_findmain] function
-#'
-#' @return A reduced data frame with important features based on the clustering
-#'   and features no clustered.
+#' @param xdataImp data matrix with samples in rows and features in columns. The
+#'   matrix should not include metadata or any columns without LC-MS data. Note
+#'   that depending on the origins of the dataset, colnames might have a
+#'   character starting with "X" avoiding a number in the first character (see
+#'   `loaded = TRUE`)
+#' @param loaded if matrix is loaded into R, an "X" will be added to each
+#'   colname. In this case, set `loaded = TRUE`.
+#' @return A list containing:
+#' $singletons: data frame with features that has not been clustered.
+#' $xdata_mol_ions: data frame with the representative ions of each cluster
+#' $xdata_reduced: data frame with the reduced dataset (singletons +
+#' representative ions of each cluster)
 #' @export
 #' @usage
-#' feature_reduction(mdata,
-#' representative_ions,
-#' RC)
-#'
-feature_reduction <- function (mdata, representative_ions, RC) {
-  mdata <- as.matrix(mdata)
-
-  # Get mz columns for the feature table
-  mz <- colnames(mdata) %>%
-    stringr::str_split(., "\\_") %>%
-    lapply(., function(x)
-      x[1]) %>%
+#' feature_reduction(RC,
+#' xdata,
+#' loaded = TRUE)
+feature_reduction <- function (RC, xdataImp, loaded = FALSE) {
+  # Selection of max intensity ion as cluster representative
+  Max_int<- lapply(RC$M.ann, function(x) x[which.max(x$int), ])
+  Molecular_ions <- dplyr::bind_rows(Max_int)
+  Molecular_ions$name <- paste(round(Molecular_ions$mz,4),
+                               round(RC$clrt,2),
+                               sep = "_")
+  # Get mz and rt columns for the feature table
+  if (loaded == TRUE){
+  mz <- colnames(xdataImp) %>%
+    stringr::str_split(.,"\\_") %>%
+    lapply(.,function(x) x[1]) %>%
     unlist() %>%
+    substring(., 2) %>% # add if xdata was loaded (cols with an X)
     as.numeric()
-  # Ramcluster make a round of 4 decimals, we have to round mz too
-  mz <- round(mz, 4)
+  } else {
+    mz <- colnames(xdataImp) %>%
+      stringr::str_split(.,"\\_") %>%
+      lapply(.,function(x) x[1]) %>%
+      unlist() %>%
+      # substring(., 2) %>% # add if xdata was loaded (cols with an X)
+      as.numeric()
+  }
+
+  mz %in% Molecular_ions$mz %>% table()
+  message("length of features: ", length(mz))
 
   # Clustered features
-  xdata_cluster_ions <- mdata[, mz %in% representative_ions$mz]
+  xdata_mol_ions <- xdataImp[, mz %in% Molecular_ions$mz]
 
   # Singletons
-  clustered_mz <- lapply(RC$M.ann,
-                         function (x)
-                           x$mz) %>% unlist() %>%  as.numeric()
-
-  mdata <- as.data.frame(mdata)
-  xdata_cluster_ions <- as.data.frame(xdata_cluster_ions)
-
-  singletons <- mdata[, !mz %in% clustered_mz]
+  clustered_mz<- lapply(RC$M.ann,
+                        function (x) x$mz) %>%
+    unlist() %>%
+    as.numeric()
 
   message("A number of ",
-          RC$nsing,
+          length(clustered_mz),
+          " features have been clustered into ",
+          dim(xdata_mol_ions)[2],
+          " representative features")
+  xdataImp <- as.data.frame(xdataImp)
+  xdata_mol_ions <- as.data.frame(xdata_mol_ions)
+  singletons <- xdataImp[,!mz %in% clustered_mz]#check xdata[,-1]
+
+  message("A number of ",
+          ncol(singletons),
           " features correspond to singletons")
   #Combine singletons and molecular ions
-  xdata_reduced <- cbind.data.frame(xdata_cluster_ions, singletons)
+  xdata_reduced <- cbind.data.frame(xdata_mol_ions, singletons)
 
   message("Original dataset has ",
-          ncol(mdata),
+          ncol(xdataImp),
           " features")
+  message("")
 
-  message("Cluster representative ions dataset has ",
-          ncol(xdata_cluster_ions),
+  message("Molecular ions dataset has ", ncol(xdata_mol_ions),
           " features")
-
   message("Singletons dataset has ", ncol(singletons), " features")
-
   message("Reduced dataset has ", ncol(xdata_reduced), " features")
-  return(xdata_reduced)
+
+  return(list(singletons = singletons, xdata_mol_ions = xdata_mol_ions, xdata_reduced = xdata_reduced))
 }
+
+
+
+#   # Abducts of representative ions
+#   Representative_adducts <- sapply(RC$M.ann, function(x) x[which.max(x$int), ]$adduct)
+#
+#   # Selection of labelled max intensity ion as cluster representative
+#   Labelled_int <- lapply(RC$M.ann, function(x) {
+#     xl <- x[which(!is.na(x$label)), ]
+#     xl[which.max(xl$int), ]
+#   })
+#   labelled_ions <- dplyr::bind_rows(Labelled_int)
+#
+#   # We save most important cluster data
+#   cluster_data <- data.frame(cluster = RC$ann,
+#                              Max_int_ion_mz = representative_ions$mz,
+#                              Max_int_ion_adduct = Representative_adducts,
+#                              Labelled_ion_mz = labelled_ions$mz,
+#                              Labelled_ion_adduct = labelled_ions$label,
+#                              RC_mz = RC$M,
+#                              retention_time = RC$clrt)
+#
+#   # All labelled ions
+#   all_labelled_adducts <- lapply(RC$M.ann, function(x) {
+#     xl <- x[which(!is.na(x$label)), ]
+#   })
+#   all_labelled_adducts <- dplyr::bind_rows(all_labelled_adducts)
+#   return(list(all_labelled_adducts = all_labelled_adducts, labelled_ions = labelled_ions, representative_ions = representative_ions, cluster_data = cluster_data))
+
+
+# feature_reduction <- function (mdata, representative_ions, RC) {
+#   mdata <- as.matrix(mdata)
+#
+#   # Get mz columns for the feature table
+#   mz <- colnames(mdata) %>%
+#     stringr::str_split(., "\\_") %>%
+#     lapply(., function(x)
+#       x[1]) %>%
+#     unlist() %>%
+#     as.numeric()
+#
+#   # Ramcluster make a round of 4 decimals, we have to round mz too
+#   mz <- round(mz, 4)
+#   representative_ions_mz <- round(representative_ions$mz, 4)
+#
+#   # Clustered features
+#   xdata_cluster_ions <- mdata[, mz %in% representative_ions_mz]
+#
+#   # Singletons
+#   clustered_mz <- lapply(RC$M.ann,
+#                          function (x)
+#                            x$mz) %>% unlist() %>%  as.numeric()
+#
+#   mdata <- as.data.frame(mdata)
+#   xdata_cluster_ions <- as.data.frame(xdata_cluster_ions)
+#
+#   singletons <- mdata[, !mz %in% clustered_mz]
+#
+#   message("A number of ",
+#           RC$nsing,
+#           " features correspond to singletons")
+#
+#   #Combine singletons and molecular ions
+#   xdata_reduced <- cbind.data.frame(xdata_cluster_ions, singletons)
+#
+#   message("Original dataset has ",
+#           ncol(mdata),
+#           " features")
+#
+#   message("Cluster representative ions dataset has ",
+#           ncol(xdata_cluster_ions),
+#           " features")
+#
+#   message("Singletons dataset has ", ncol(singletons), " features")
+#
+#   message("Reduced dataset has ", ncol(xdata_reduced), " features")
+#   return(xdata_reduced)
+# }
--- R/pre_processing.R
+++ R/pre_processing.R
@@ -1,16 +1,11 @@
 #' Filter an experiment by its polarity
 #'
-#' This function was merged in
-#' the MSnbase package on 2018-12-10 as proposed at:
-#'
-#' https://github.com/lgatto/MSnbase/issues/388
-#'
-#' Once MSnbase is released (Bioconductor 3.9, maybe once R 3.6 is out),
-#' remove this function.
-#'
+#' This function was merged in the MSnbase package on 2018-12-10. It filter by
+#' positive or negative ionization mode.
 #'
 #' @param lcms_dataset A MSnExp object.
-#' @param polarity. The polarity to keep.
+#' @param polarity. The polarity to keep. set `polarity. = 1` for positive
+#'   ionization mode. set `polarity. = 0` for negative ionization mode.
 #' @family dataset functions
 #' @family filtering functions
 #' @return A filtered lcms_dataset with the selected polarity.
